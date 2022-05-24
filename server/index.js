1a29e7e737d30571f7a16edd9b373661a9040f40
“lalitsaini85” <lalit.saini_cs19@gla.ac.in>
2022-05-25 01:37:05 +0530

    connect to mongodg url

--- server/index.js
+++ server/index.js
@@ -2,3 +2,11 @@ import { Express } from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from 'cors';
+
+const app =  express();
+app.use(bodyParser.json({'limit':"30mb", extended:true}));
+app.use(bodyParser.urlencoded({'limit':"30mb", extended:true}));
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://rewind-memories:rewind@123@cluster0.ovgypox.mongodb.net/?retryWrites=true&w=majority";
