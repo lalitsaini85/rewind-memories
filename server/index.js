3e70177c0872b79e4d3750bffc0efa75deb5576b
“lalitsaini85” <lalit.saini_cs19@gla.ac.in>
2022-05-26 02:50:53 +0530

    add models, controllers, and routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
-import { Express } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app =  express();
+app.use('/posts', postRoutes);
+
 app.use(bodyParser.json({'limit':"30mb", extended:true}));
 app.use(bodyParser.urlencoded({'limit':"30mb", extended:true}));
 
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://rewind-memories:rewind@123@cluster0.ovgypox.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = 'mongodb+srv://rewind-memories:rewind%40123@cluster0.ovgypox.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>app.listen(PORT,()=>console.log(`server running on port: ${PORT}`)))
+.catch((error)=>console.log(error.message));
+
+
+
